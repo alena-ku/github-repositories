16f4ec87aa213a894bf24d3aeb30f092ee3b7343
Alena Ku <alena.ku.xxx@gmail.com>
2019-10-12 19:46:51 +0300

    Implement repositories response handling: progress and alert dialog

--- app/src/main/java/com/example/githubtest/ui/activities/MainActivity.kt
+++ app/src/main/java/com/example/githubtest/ui/activities/MainActivity.kt
@@ -1,10 +1,15 @@
 package com.example.githubtest.ui.activities
 
-import androidx.appcompat.app.AppCompatActivity
+import android.app.AlertDialog
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
 import com.example.githubtest.R
 import com.example.githubtest.model.GitHubRepositoriesRepository
 import dagger.android.AndroidInjection
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.schedulers.Schedulers
+import kotlinx.android.synthetic.main.activity_main.*
 import javax.inject.Inject
 
 class MainActivity : AppCompatActivity() {
@@ -16,5 +21,33 @@ class MainActivity : AppCompatActivity() {
         AndroidInjection.inject(this)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        showRepositoriesButton.setOnClickListener {
+            toggleLoading(true)
+
+            //unsubscribeOnDestroy
+            gitHubRepositoriesRepository.getByUserName(userTextInputEditText.text.toString())
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe({
+                    toggleLoading(false)
+
+                }, { error ->
+                    loadingFailed(error.localizedMessage)
+                    toggleLoading(false)
+                })
+        }
+    }
+
+    fun loadingFailed(message: String) {
+        AlertDialog.Builder(this)
+            .setTitle(R.string.app_name)
+            .setMessage(message)
+            .setPositiveButton(getString(R.string.ok), null)
+            .show()
+    }
+
+    fun toggleLoading(loading: Boolean) {
+        progressBar.visibility = if (loading) View.VISIBLE else View.GONE
     }
 }
