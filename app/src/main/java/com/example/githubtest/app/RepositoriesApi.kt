505d05b8c841c9bb4a4570811a4d84d3815e27fd
Alena Ku <alena.ku.xxx@gmail.com>
2019-10-12 18:21:09 +0300

    Add repositories api, retrofit and rxjava

---
+++ app/src/main/java/com/example/githubtest/app/RepositoriesApi.kt
@@ -0,0 +1,11 @@
+package com.example.githubtest.app
+
+import com.example.githubtest.models.GitHubRepository
+import io.reactivex.Single
+import retrofit2.http.GET
+import retrofit2.http.Path
+
+interface RepositoriesApi {
+    @GET("users/{userName}/repos")
+    fun requestUserRepositories(@Path("userName") userName: String): Single<List<GitHubRepository>>
+}
