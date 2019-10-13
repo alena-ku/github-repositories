f70958700331ae970c1964449c06e9fb6b24143c
Alena Ku <alena.ku.xxx@gmail.com>
2019-10-13 22:12:12 +0300

    Implement sorting on repos list

--- app/src/main/java/com/example/githubtest/ui/activities/RepositoriesActivity.kt
+++ app/src/main/java/com/example/githubtest/ui/activities/RepositoriesActivity.kt
@@ -3,22 +3,31 @@ package com.example.githubtest.ui.activities
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
-import androidx.recyclerview.widget.DividerItemDecoration
 import com.example.githubtest.R
 import com.example.githubtest.model.models.GitHubRepository
 import com.example.githubtest.ui.RepositoriesAdapter
 import com.google.gson.Gson
-
 import kotlinx.android.synthetic.main.activity_repositories.*
+
 
 class RepositoriesActivity : AppCompatActivity(), RepositoriesAdapter.Interaction {
 
-    companion object{
-        private const val ARGS_REPOSITORIES = "repositories"
-        private const val ARGS_USERNAME = "username"
+    private lateinit var gitHubRepositories: List<GitHubRepository>
+    private lateinit var repositoriesAdapter: RepositoriesAdapter
 
-        fun createIntent(context: Context, username: String, gitHubRepositories: List<GitHubRepository>) =
+    companion object {
+        private const val ARGS_USERNAME = "username"
+        private const val ARGS_REPOSITORIES = "repositories"
+        private const val ARGS_SORT_BY_CREATED = "sortByCreated"
+
+        fun createIntent(
+            context: Context,
+            username: String,
+            gitHubRepositories: List<GitHubRepository>
+        ) =
             Intent(context, RepositoriesActivity::class.java).apply {
                 putExtra(ARGS_USERNAME, Gson().toJson(username))
                 putExtra(ARGS_REPOSITORIES, Gson().toJson(gitHubRepositories))
@@ -29,25 +38,60 @@ class RepositoriesActivity : AppCompatActivity(), RepositoriesAdapter.Interactio
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_repositories)
         setSupportActionBar(toolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
-        var gitHubRepositories = Gson().fromJson(
+        loadDataFromIntent()
+    }
+
+    private fun loadDataFromIntent() {
+        var username = Gson().fromJson(
+            intent.getStringExtra(ARGS_USERNAME),
+            String::class.java
+        )
+
+        gitHubRepositories = Gson().fromJson(
             intent.getStringExtra(ARGS_REPOSITORIES),
             Array<GitHubRepository>::class.java
         ).asList()
 
-        var username = Gson().fromJson(
-            intent.getStringExtra(ARGS_USERNAME),
-            String::class.java)
-
-        val repositoriesAdapter = RepositoriesAdapter(username, this)
+        repositoriesAdapter = RepositoriesAdapter(username, this)
         repositoriesRecyclerView.adapter = repositoriesAdapter
-        repositoriesAdapter.submitList(gitHubRepositories)
 
-        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        setSortByCreated(intent.getBooleanExtra(ARGS_SORT_BY_CREATED, true))
     }
 
     override fun itemClicked(item: GitHubRepository) {
         startActivity(RepositoryDetailsActivity.createIntent(this, item))
     }
 
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_repos, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.action_sort_created -> {
+                setSortByCreated(true)
+                true
+            }
+            R.id.action_sort_name -> {
+                setSortByCreated(false)
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    private fun setSortByCreated(sortByCreated: Boolean) {
+        intent.putExtra(ARGS_SORT_BY_CREATED, sortByCreated)
+
+        val list =
+            if (sortByCreated)
+                gitHubRepositories.sortedBy { it.createdAt }
+            else
+                gitHubRepositories.sortedBy { it.name?.toLowerCase() }
+
+        repositoriesAdapter.submitList(list)
+    }
 }
